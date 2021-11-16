f15c27801dcdedd573ca0ad3b3ab19d079f91783
umurgdk <umurgdk@gmail.com>
2021-11-16 20:00:02 +0300

    Initial Commit

---
+++ Web Grid/Web_GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  Web_GridApp.swift
+//  Web Grid
+//
+//  Created by Umur Gedik on 16.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Web_GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
